013c606e24d4c5fa54a69675e6749d729f1bada9
Edwxd <101204772+Edwxd@users.noreply.github.com>
2023-10-11 10:46:56 -0400

    Add project files.

---
+++ Lab5/FileReader.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WordUnscrambler
+{
+    class FileReader
+    {
+        public string[] Read(string filename)
+        {
+
+
+            string filePath = "C: \\Users\\edwar\\Downloads\\WordUnscrambler_Startup_Code\\WordUnscrambler_Startup_Code\\WordFile"; 
+
+            try
+            {
+                string content = File.ReadAllText(filePath);
+                //Console.WriteLine("File content:\n" + content);
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("An error occurred: " + e.Message);
+            }
+
+           
+            return null;
+        }
+    }
+}
