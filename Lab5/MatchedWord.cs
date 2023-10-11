e8067a1b43a0eef66904426e74bd24b0cd43c04a
Tyler Foo <2130579@champlaincollege.qc.ca>
2023-10-11 11:12:05 -0400

    BuildMatchedWords constructor added

--- Lab5/MatchedWord.cs
+++ Lab5/MatchedWord.cs
@@ -11,4 +11,17 @@ namespace WordUnscrambler
         public string ScrambledWord { get; set; }
         public string Word { get; set; }
     }
+
+    //Constructor to build matched word
+    public MatchedWord(string scrambledWord, string word)
+    {
+        ScrambledWord = scrambledWord;
+        Word = word;
+    }
+
+    //ToString to output matching words
+    public override string ToString()
+    {
+        return $"{this.Word} matches {this.ScrambledWord}";
+    }
 }
